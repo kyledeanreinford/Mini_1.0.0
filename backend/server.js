9824e8445ad450673cbd60d9519b1be8dd5917b4
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2021-08-27 16:00:58 -0500

    moved frontend build again

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const PORT = process.env.PORT;
 
-app.use(express.static("../frontend/build"));
+app.use(express.static("build"));
 
 app.use(express.json());
 app.use(cors());
